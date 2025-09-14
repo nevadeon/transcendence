6dab62cfed44024c24e76791358276792d6e1a0d
paul_mallet <paulhavre42@gmail.com>
2025-09-14 12:51:11 +0200

    added background & forms styles, still need to fix sounds

--- services/frontend/src/main.tsx
+++ services/frontend/src/main.tsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router/dom'
 import router from './routes/Router.tsx'
+import './assets/start.mp3'
 import './styles/index.css'
+import './styles/Auth.css'
 
 
 createRoot(document.getElementById('root')!).render(
