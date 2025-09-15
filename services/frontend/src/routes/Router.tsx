0b0b9e4da3dd2254aabc103066fc8504b6e3ad29
Paul Mallet <pamallet@c2r12p4.42lehavre.fr>
2025-09-15 17:32:17 +0200

    added private routes, still need auth verification

--- services/frontend/src/routes/Router.tsx
+++ services/frontend/src/routes/Router.tsx
@@ -1,12 +1,19 @@
-import { createBrowserRouter } from "react-router"
+import { createBrowserRouter } from "react-router";
+import PrivateRoute from "../components/PrivateRoute";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
+import Home from "../pages/Home";
 import App from "../components/App";
 
 const router = createBrowserRouter([
 	{ path: "/", element: <App /> },
 	{ path: "/register", element: <Register /> },
 	{ path: "/login", element: <Login /> },
+	{ element: <PrivateRoute />,
+		children: [
+			{ path: "/home", element: <Home /> },
+		]
+	},
 ])
 
 export default router;
