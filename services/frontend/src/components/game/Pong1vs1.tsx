2ace59d7060384781ef0fff8a744799cb579fc4d
paul_mallet <paulhavre42@gmail.com>
2025-09-25 11:13:34 +0200

    added basic padsmove logic, still need to fix max and min height values based on resize

--- services/frontend/src/components/game/Pong1vs1.tsx
+++ services/frontend/src/components/game/Pong1vs1.tsx
@@ -3,9 +3,16 @@ import Player2Source from "../../assets/avatars/poopy.png";
 import ArenaSource from "../../assets/game/arena.svg";
 import PadSource from "../../assets/game/pad.svg";
 import BallSrc from "../../assets/game/balls/morty's_head_ball.png";
+import ArrowUpSrc from "../../assets/game/keybinds/arrow-up.svg";
+import ArrowDownSrc from "../../assets/game/keybinds/arrow-down.svg";
+import usePadsMove from "../../hooks/usePadsMove";
 
 // /game/1vs1
 export default function Pong1vs1() {
+	const ctrl1 = { upKey: 'q', downKey: 'a' };
+	const ctrl2 = { upKey: 'o', downKey: 'l' };
+	const { pad1Pos, pad2Pos } = usePadsMove(ctrl1, ctrl2);
+
 	return (
 		<div className="pong-1vs1">
 			<div className="pong-1vs1-scores">
@@ -31,9 +38,9 @@ export default function Pong1vs1() {
 			<div className="pong-1vs1-arena">
 				<img src={ArenaSource} alt="Arena Image" className="pong-1vs1-arena-zone" />
 				<div className="pong-1vs1-arena-playzone">
-					<img src={PadSource} alt="Pad Image" className="pad left" />
+					<img src={PadSource} alt="Pad Image" className="pad left" style={{transform: `translateY(${pad1Pos - 50}%)`}} />
 					<img src={BallSrc} alt="Morty Ball" className="ball" />
-					<img src={PadSource} alt="Pad Image" className="pad" />
+					<img src={PadSource} alt="Pad Image" className="pad" style={{transform: `translateY(${pad2Pos - 50}%)`}} />
 				</div>
 			</div>
 			<div className="pong-1vs1-scores">
@@ -47,11 +54,15 @@ export default function Pong1vs1() {
 				</div>
 				<div className="pong-1vs1-shortcuts">
 					<div className="pong-1vs1-shortcuts-key">
-						<span>A</span>
+						<span>
+							<img src={ArrowUpSrc} alt="Arrow Up Icon" />
+							</span>
 						<span>UP</span>
 					</div>
 					<div className="pong-1vs1-shortcuts-key">
-						<span>Q</span>
+						<span>
+							<img src={ArrowDownSrc} alt="Arrow Down Icon" />
+						</span>
 						<span>DOWN</span>
 					</div>
 				</div>
---
+++ services/frontend/src/hooks/usePadsMove.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect, useRef } from "react";
+import { PAD_SPEED, type PadControls, type PadPositions } from "../interfaces/Pads";
+
+export default function usePadsMove(ctrlPlayer1: PadControls, ctrlPlayer2: PadControls): PadPositions {
+	const [ pad1Pos, setPad1Pos ] = useState<number>(50);
+	const [ pad2Pos, setPad2Pos ] = useState<number>(50);
+	const keysPressed = useRef<{ [key: string]: boolean }>({});
+	const animRef = useRef<number>(0);
+
+	useEffect(() => {
+		const handleKeyUp = (e: KeyboardEvent) => {
+			keysPressed.current[e.key.toLowerCase()] = true;
+		};
+		const handleKeyDown = (e: KeyboardEvent) => {
+			keysPressed.current[e.key.toLowerCase()] = false;
+		};
+		window.addEventListener('keyup', handleKeyUp);
+		window.addEventListener('keydown', handleKeyDown);
+		return () => {
+			window.removeEventListener('keyup', handleKeyUp);
+			window.removeEventListener('keydown', handleKeyDown);
+		}
+	}, []);
+
+	useEffect(() => {
+		const gameLoop = () => {
+			setPad1Pos(prevPos => {
+				let newPos = prevPos;
+				if (keysPressed.current[ctrlPlayer1.upKey.toLowerCase()])
+					newPos -= PAD_SPEED;
+				if (keysPressed.current[ctrlPlayer1.downKey.toLowerCase()])
+					newPos += PAD_SPEED;
+				return Math.max(-150, Math.min(250, newPos));
+			});
+			setPad2Pos(prevPos => {
+				let newPos = prevPos;
+				if (keysPressed.current[ctrlPlayer2.upKey.toLowerCase()])
+					newPos += PAD_SPEED;
+				if (keysPressed.current[ctrlPlayer2.downKey.toLowerCase()])
+					newPos -= PAD_SPEED;
+				return Math.max(0, Math.min(100, newPos));
+			});
+			animRef.current = requestAnimationFrame(gameLoop);
+		};
+		animRef.current = requestAnimationFrame(gameLoop);
+		return () => {
+			cancelAnimationFrame(animRef.current!);
+		};
+	}, [ctrlPlayer1, ctrlPlayer2]);
+
+	return { pad1Pos, pad2Pos };
+}
