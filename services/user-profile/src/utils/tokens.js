20ee11726856397effae6d7d2a6a3ee0b96e4307
Hp de moa <taquettom3@gmail.com>
2025-09-27 15:21:37 +0200

    ~ | Updated saveToken to removed token from the same user

--- services/user-profile/src/utils/tokens.js
+++ services/user-profile/src/utils/tokens.js
@@ -1,4 +1,6 @@
 export async function saveToken(db, username, token) {
+	const tmp = await db.get("SELECT token FROM tokens WHERE username = ?", [username]);
+	if (tmp) await deleteToken(db, tmp.token);
 	await db.run(
 		"INSERT INTO tokens (username, token, expires_at) VALUES (?, ?, datetime('now', '+1 hour'))",
 		[username, token]
