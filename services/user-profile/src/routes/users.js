19b7bae50a0d6cb9f47b07b32cc73d1f523a6ca1
Hp de moa <taquettom3@gmail.com>
2025-10-02 19:21:11 +0200

    ~ | Updated two_factor put request

--- services/user-profile/src/routes/users.js
+++ services/user-profile/src/routes/users.js
@@ -1,22 +1,19 @@
 async function userRoutes(fastify) {
 	const { db } = fastify;
 
-	fastify.get("/users", async () => {
-		return await db.all("SELECT * FROM users");
-	});
-
 	fastify.get("/users/:id/", async (req, reply) => {
-		const user = await db.get("SELECT * FROM users WHERE id = ?", [req.params.id]);
+		const { id } = req.body;
+		const user = await db.get("SELECT * FROM users WHERE id = ?", [id]);
 		if (!user) return reply.code(404).send({ error: "User not found" });
 		return reply.code(201).send({user: user});
 	});
 
 	fastify.put("/users/:id/name", async (req, reply) => {
-		const { name } = req.body;
+		const { name, id } = req.body;
 		try {
 			const result = await db.run(
 				"UPDATE users SET name=?, WHERE id=?",
-				[name, req.params.id]
+				[name, id]
 			);
 			if (result.changes === 0)
 				return reply.code(404).send({ error: "Username has not change." });
@@ -36,11 +33,11 @@ async function userRoutes(fastify) {
 	});
 
 	fastify.put("/users/:id/email", async (req, reply) => {
-		const { email } = req.body;
+		const { email, id} = req.body;
 		try {
 			const result = await db.run(
 				"UPDATE users SET email=?, WHERE id=?",
-				[email, req.params.id]
+				[email, id]
 			);
 			if (result.changes === 0)
 				return reply.code(404).send({ error: "Email has not change." });
@@ -60,11 +57,11 @@ async function userRoutes(fastify) {
 	});
 
 	fastify.put("/users/:id/password", async (req, reply) => {
-		const { password } = req.body;
+		const { password, id } = req.body;
 		try {
 			const user = await db.get(
 				"SELECT password FROM users WHERE id = ?",
-				[req.params.id]
+				[id]
 			);
 			const test = await argon2.verify(user.password, password);
 			if (test)
@@ -72,7 +69,7 @@ async function userRoutes(fastify) {
 
 			const hashed_password = await argon2.hash(password);
 			await db.run(
-				"UPDATE users SET password = ? WHERE id = ?", [hashed_password, req.params.id]
+				"UPDATE users SET password = ? WHERE id = ?", [hashed_password, id]
 			);
 			return reply.code(201);
 		} catch (err) {
@@ -81,8 +78,28 @@ async function userRoutes(fastify) {
 		}
 	});
 
+		fastify.put("/users/:id/two_factor", async (req, reply) => {
+		const { id } = req.body;
+		try {
+			const two_factor = await db.get(
+				"SELECT two_factor FROM users WHERE ID = ?", [id]
+			);
+			two_factor = (!(two_factor === 1));
+			const result = await db.run(
+				"UPDATE users SET two_factor=?, WHERE id=?",
+				[two_factor, id]
+			);
+			return reply.code(201).send({ two_factor: two_factor });
+		} catch (err) {
+			fastify.log.error(err);
+			return reply.code(500).send(err);
+		}
+	});
+
+
 	fastify.delete("/users/:id", async (req, reply) => {
-		const result = await db.run("DELETE FROM users WHERE id=?", [req.params.id]);
+		const { id } = req.body;
+		const result = await db.run("DELETE FROM users WHERE id=?", [id]);
 		if (result.changes === 0) return reply.code(404).send({ error: "User not found" });
 		return { message: "Deleted successfully" };
 	});
