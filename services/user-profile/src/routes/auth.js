48a047a293ea70c205ea7a096945cb976ec575c7
paul_mallet <paulhavre42@gmail.com>
2025-10-04 19:41:26 +0200

    added jwtDecode and logic to send proper google token to backend, then implement a specific route for verif, connexion or incription and then insertion in the db if new user registered

--- services/user-profile/src/routes/auth.js
+++ services/user-profile/src/routes/auth.js
@@ -1,7 +1,11 @@
-import argon2 from "argon2";
+// import { OAuth2Client } from 'google-auth-library';
 import { deleteToken, saveToken } from "../utils/tokens.js";
 import { getVaultSecret } from "../plugins/vault.js";
+import argon2 from "argon2";
 import crypto from 'crypto';
+
+// const GOOGLE_CLIENT_ID = await getVaultSecret("user-profile/config", "GOOGLE_CLIENT_ID");
+// const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
 
 async function authRoutes(fastify) {
 	const { db, auth } = fastify;
@@ -48,6 +52,58 @@ async function authRoutes(fastify) {
 			return reply.code(500).send({ field: "password", msg: "SERVER ERROR" });
 		}
 	});
+
+	// async function issueUserSession(reply, user) {
+    //     // user doit contenir l'id et le name
+    //     const token = auth.generateLongToken(user);
+    //     await saveToken(db, user.name, token, '+1 hour');
+    //     const user_data = await db.get( "SELECT id, name, species, planet, dimension, avatar FROM users WHERE id=?", [user.id] );
+    //     return reply.code(201).send({ user: user_data, token });
+    // }
+
+	// fastify.post("/register/google/verify", async (req, reply) => {
+    //     const { idToken } = req.body; // Token envoyé par le frontend
+    //     if (!idToken)
+    //         return reply.code(400).send({ message: "ID Token Google manquant." }); // #TODO console.error OU <Register />(page) recup et affiche in <Form />
+    //     try {
+    //         // 1. VÉRIFICATION SÉCURISÉE DU TOKEN GOOGLE
+    //         const ticket = await googleClient.verifyIdToken({ idToken: idToken, audience: GOOGLE_CLIENT_ID });
+	// 		const payload = ticket.getPayload();
+    //         if (!payload || !payload.sub || !payload.email)
+    //             return reply.code(401).send({ message: "Token invalide ou données manquantes." }); //RESPONSE
+
+	// 		const googleId = payload.sub; // googleId unique
+    //         const email = payload.email;
+    //         const name = payload.name || payload.given_name || 'GoogleUser';
+	// 		// 2. GESTION CONNEXION / INSCRIPTION (Upsert)
+	// 		let user = await db.get("SELECT * FROM users WHERE googleId=?", [googleId]);
+    //         if (user)
+    //             return issueUserSession(reply, user); //RESPONSE
+
+	// 		user = await db.get("SELECT * FROM users WHERE email=?", [email]); // si deja log via credentials, et veux OAuth ensuite
+	// 		if (user) {
+    //             await db.run("UPDATE users SET googleId=? WHERE id=?", [googleId, user.id]);
+    //             return issueUserSession(reply, user); //RESPONSE
+    //         } else {
+    //             // INSCRIPTION: Nouvel utilisateur, l'enregistrer dans la DB
+    //             const SECRET_SALT = await getVaultSecret("user-profile/config", "SECRET_SALT");
+    //             const hashed_email = crypto.createHash('sha256').update(email + SECRET_SALT).digest('hex');
+    //             // Insérer le new user (sans mot de passe, mais avec googleId)
+    //             await db.run(
+    //                 "INSERT INTO users(name, email, googleId) VALUES(?, ?, ?)",
+    //                 [name, hashed_email, googleId]
+    //             );
+    //             const newUser = await db.get("SELECT * FROM users WHERE googleId=?", [googleId]);
+    //             if (newUser)
+    //                 return issueUserSession(reply, newUser); //RESPONSE
+    //             else
+    //                 throw new Error("Erreur lors de la récupération du nouvel utilisateur."); //RESPONSE(to catch)
+    //         }
+	// 	} catch (err) {
+    //         fastify.log.error("Erreur Google Auth:", err.message);
+    //         return reply.code(401).send({ message: "Authentification via Google échouée." }); //RESPONSE
+    //     }
+    // });
 
 	// Login
 	fastify.post("/login", async (req, reply) => {
