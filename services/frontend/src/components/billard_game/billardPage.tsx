b57fae6a3d5ccd81f225d9c8672f9f5dd68b047c
Hp de moa <taquettom3@gmail.com>
2025-10-08 21:36:58 +0200

    + | Added billard without pamallet changes

--- services/frontend/src/components/billard_game/billardPage.tsx
+++ services/frontend/src/components/billard_game/billardPage.tsx
@@ -2,11 +2,11 @@ export default function GamePage() {
 	return (
 		<div className="game-container">
 			<iframe
-				src="/billiard_game/index.html"
+				src="/billard_game/index.html"
 				width="800"
 				height="600"
 				style={{ border: "none" }}
-				title="billiard Game"
+				title="Billard Game"
 				allowFullScreen
 			/>
 		</div>
