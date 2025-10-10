523220348304ab8195ade3df84227be93253f0b5
Hp de moa <taquettom3@gmail.com>
2025-10-10 14:42:03 +0200

    merge with main

--- services/frontend/src/components/board/body-games/Stats.tsx
+++ services/frontend/src/components/board/body-games/Stats.tsx
@@ -7,7 +7,19 @@ import "../../../styles/board/body-games/Stats.css";
 
 export interface GamesProps {
 	id: number,
-	// based on game/db.js
+	left_team_A: string,
+	avatar_left_team_A: string,
+	left_team_B: string | null,
+	avatar_left_team_B: string | null,
+	right_team_A: string,
+	avatar_right_team_A: string,
+	right_team_B: string | null,
+	avatar_right_team_B: string | null,
+	mode: string,
+	left_team_score: number,
+	right_team_score: number,
+	winnerId: number,
+	date: string
 }
 
 export default function Stats(props: StatsProps) {
@@ -23,7 +35,7 @@ export default function Stats(props: StatsProps) {
 	useEffect(() => {
 		if (!isOpen || !token || !user)
 			return ;
-		async function allUsers() {
+		async function allGames() {
 			setIsLoading(true);
 			try {
 				const res = await fetch(`http://localhost:3002/match_history/${user.name}`, {
@@ -47,8 +59,8 @@ export default function Stats(props: StatsProps) {
 			}
 		}
 		if (isOpen)
-			allUsers();
-	}, [isOpen, token]);
+			allGames();
+	}, [isOpen, token, user]);
 
 	// const statsByMode = games.reduce((acc: StatsByMode, curr) => {
 	// 	const mode = curr.mode;
@@ -72,6 +84,9 @@ export default function Stats(props: StatsProps) {
 
 	if (!isOpen)
 		return null;
+
+	if (isLoading)
+		return "Loading history game...";
 
 	return (
 		<div className="stats">
@@ -102,31 +117,24 @@ export default function Stats(props: StatsProps) {
 							</th>
 						</tr>
 					</thead>
-					{/* See Trans3 in Gemini paulhavre42 account
-						<div className="scroll-wrapper">
-							<table className="body-table">
-								tbody
-							</table>
-						</div>
-					*/}
 					<tbody>
-						{/* {games.map((game) => (
+						{gameHistory.map((game) => (
 							<tr key={game.id}>
 								<td> {game.mode} </td>
-								<td> {game.scores.score1} - {game.scores.score2} </td>
+								<td> {game.left_team_score} - {game.right_team_score} </td>
 								<td>
 									<div className="opponents-container">
 										<div className="opponent1">
-											<img src={game.opponent1.avatar} alt="Opponent Avatar" />
-											<span> {game.opponent1.username} </span>
+											<img src={game.avatar_right_team_A} alt="Opponent Avatar" />
+											<span> {game.right_team_A} </span>
 										</div>
 										{
-											game.opponent2 &&
+											game.right_team_B && game.avatar_right_team_B &&
 											(
 												<div className="opponent2">
 													<span>+</span>
-													<img src={game.opponent2.avatar} alt="Opponent2 Avatar" />
-													<span> {game.opponent2.username} </span>
+													<img src={game.avatar_right_team_B} alt="Opponent2 Avatar" />
+													<span> {game.right_team_B} </span>
 												</div>
 											)
 										}
@@ -134,11 +142,11 @@ export default function Stats(props: StatsProps) {
 								</td>
 								<td>
 									{
-										game.ally ?
+										game.left_team_B && game.avatar_left_team_B ?
 										(
 											<div>
-												<img src={game.ally.avatar} alt="Ally Avatar" />
-												<span> {game.ally.username} </span>
+												<img src={game.avatar_left_team_B} alt="Ally Avatar" />
+												<span> {game.left_team_B} </span>
 											</div>
 										) : "none"
 									}
